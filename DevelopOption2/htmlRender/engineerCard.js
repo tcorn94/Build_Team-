405a4acb02ff739bed1611fc3e69bc082ceac1fd
Princess Itaya <taycornelius@att.net>
2020-01-07 21:25:56 -0500

    working on html skeleton

--- DevelopOption2/htmlRender/engineerCard.js
+++ DevelopOption2/htmlRender/engineerCard.js
@@ -2,7 +2,16 @@
 // create a function that holds the html that will out put the engineer
 function engineerCard(data){
     return `
-   
+    <div class="card" style="width: 18rem;">
+    <img src="https://www.st-andrews.ac.uk/dpl/1.5.0/assets/docs/images/placeholders/200x200.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"> ${data.name}</h5>
+      <p class="card-text">Role: ${data.getRole()}.</p>
+      <p class="card-text"> ID#: ${data.id}.</p>
+      <p class="card-text">  Email: ${data.email}.</p>
+    
+    </div>
+  </div>
     
     `
 
--- DevelopOption2/htmlRender/mainRender.js
+++ DevelopOption2/htmlRender/mainRender.js
@@ -2,7 +2,30 @@
 // create a function that holds the html that will output the main body of the html and will take in the data from the other cards
 function mainRender(data){
     return `
+   
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Page</title>
+     
+      </head>
+    
+      <body>
+      <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Your Team</h1>
+        <p class="lead">A list of your teammates and their contact info.</p>
+      </div>
+    </div>
+        <script src="js/scripts.js"></script>
+      </body>
+    </html>
 
+   
 `
 }
 // export mainRender
